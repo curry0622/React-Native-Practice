b222031964097bfc367a8ecc3a25b41b98365d53
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-28 16:37:43 +0800

    feat: add profile listitem

--- src/screens/settingScreen.js
+++ src/screens/settingScreen.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView  } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View  } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
 
 const SettingScreen = () => {
   return (
     <SafeAreaView style={styles.container} >
-      <Text>Setting</Text>
+      <View style={styles.listContainer}>
+        <ListItem bottomDivider>
+          <Ionicons name="person-circle-sharp" size={24} color="#4d419e" />
+          <ListItem.Content>
+            <ListItem.Title>My Profile</ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+        <ListItem bottomDivider>
+          <ListItem.Chevron />
+        </ListItem>
+        <ListItem bottomDivider>
+          <ListItem.Chevron />
+        </ListItem>
+      </View>
     </SafeAreaView >
   )
 };
@@ -14,7 +30,10 @@ export default SettingScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    paddingVertical: 10,
+    paddingHorizontal: 10
+  },
+  listContainer: {
+    width: 'auto'
   }
 });
