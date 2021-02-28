cdb8b7cfa3b3fdbe028c518f78c1922f59b907c3
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-28 14:10:34 +0800

    feat: add home and setting screens

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screens/homeScreen';
+import SettingScreen from './src/screens/settingScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Setting" component={SettingScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
