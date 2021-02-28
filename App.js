4e66f6a40d03b1b01882024df5c65de631ea26b5
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-28 15:17:12 +0800

    feat: new screens and focused color of tab

--- App.js
+++ App.js
@@ -5,9 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/homeScreen';
+import ListScreen from './src/screens/listScreen';
+import ScanScreen from './src/screens/scanScreen'
+import NotifyScreen from './src/screens/notifyScreen';
 import SettingScreen from './src/screens/settingScreen';
 import { Entypo } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+import { Foundation } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,8 +19,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarIcon: () => <Entypo name="home" size={24} color="#4d419e" /> }} />
-        <Tab.Screen name="Setting" component={SettingScreen} options={{ tabBarIcon: () => <Ionicons name="settings-sharp" size={24} color="#4d419e" /> }} />
+        <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarIcon: ({ focused }) => <Entypo name="home" size={20} color={focused ? '#4d419e' : '#707070'} /> }} />
+        <Tab.Screen name="List" component={ListScreen} options={{ tabBarIcon: ({ focused }) => <Foundation name="list" size={20} color={focused ? '#4d419e' : '#707070'} /> }} />
+        <Tab.Screen name="Scan" component={ScanScreen} options={{ tabBarIcon: ({ focused }) => <Ionicons name="scan-circle" size={30} color={focused ? '#4d419e' : '#707070'} /> }} />
+        <Tab.Screen name="Notify" component={NotifyScreen} options={{ tabBarIcon: ({ focused }) => <Ionicons name="notifications" size={20} color={focused ? '#4d419e' : '#707070'} /> }} />
+        <Tab.Screen name="Setting" component={SettingScreen} options={{ tabBarIcon: ({ focused }) => <Ionicons name="settings-sharp" size={20} color={focused ? '#4d419e' : '#707070'} /> }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
