47c8bec7eccff0e46ecef12a8a93f7d7063e63ea
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-28 14:43:37 +0800

    feat: container styles

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView  } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <SafeAreaView style={styles.container} >
       <Text>Home</Text>
-    </View>
+    </SafeAreaView >
   )
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
--- src/screens/settingScreen.js
+++ src/screens/settingScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView  } from 'react-native';
 
 const SettingScreen = () => {
   return (
-    <View>
+    <SafeAreaView style={styles.container} >
       <Text>Setting</Text>
-    </View>
+    </SafeAreaView >
   )
 };
 
 export default SettingScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
