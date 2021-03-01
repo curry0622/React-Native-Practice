3fa07d62e6aa887028eb71dd9e4054b7c06ed197
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-03-01 14:11:41 +0800

    feat: qrcode scan and open in browser

--- src/screens/scanScreen.js
+++ src/screens/scanScreen.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { Button } from 'react-native-elements';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as WebBrowser from 'expo-web-browser';
 
-const ScanScreen = () => {
+const ScanScreen = () => {const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = async ({ type, data }) => {
+    setScanned(true);
+    setUrl(data);
+    alert(data);
+  };
+
+  const handleOpenBrowser = async () => {
+    WebBrowser.dismissBrowser()
+    const result = await WebBrowser.openBrowserAsync(url);
+    setUrl(null);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>ScanScreen</Text>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={styles.scanner}
+      />
+      <Button title="Tap to Scan Again" disabled={!scanned} onPress={() => setScanned(false)} />
+      <Button title="Open in browser" disabled={!url} onPress={handleOpenBrowser} />
     </SafeAreaView>
   )
 };
@@ -14,7 +52,11 @@ export default ScanScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center'
+  },
+  scanner: {
+    width: 300,
+    height: 300
   }
 });
